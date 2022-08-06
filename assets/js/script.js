49c697655995507e3119a87455745d13e97d3f9a
Stan Sitnitski <stanley@Stans-MBP.socal.rr.com>
2022-08-06 15:25:34 -0700

    competed code  w/o readme but needs clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,41 @@
 $(document).ready(function () {
-    //Moment.js code for current date and time
-    let currentDay = moment().format("MMMM Do, YYYY");
-    let displayDate = document.getElementById("currentDay");
-    displayDate.innerHTML = currentDay;
-    let currentHour = moment().format("HH");
+  //Moment.js code for current date and time
+  let currentDay = moment().format("MMMM, DD, YYYY");
+  let displayDate = document.getElementById("currentDay");
+  displayDate.innerHTML = currentDay;
+  let currentHour = moment().hour();
+
+  $(".time-div").each(function () {
+      let timeDiv = parseInt($(this).attr("id").split('-')[1]);
+      
+      if (timeDiv < currentHour) {
+        $(this).addClass("past");
+        
+      } else if (timeDiv === currentHour) {
+        $(this).addClass("present");
+      
+      } else {
+        $(this).addClass("future");
+      }
+    });
+
+    $(".saveBtn").click(function (event) {
+      event.preventDefault();
+      let value = $(this).siblings(".time-block").val();
+      let time = ($(this).parent().attr("id"));
+      localStorage.setItem(time, value);
+    });
+
+  $("#h-9 .time-block").val(localStorage.getItem("h-9"));
+  $("#h-10 .time-block").val(localStorage.getItem("h-10"));
+  $("#h-11 .time-block").val(localStorage.getItem("h-11"));
+  $("#h-12 .time-block").val(localStorage.getItem("h-12"));
+  $("#h-13 .time-block").val(localStorage.getItem("h-13"));
+  $("#h-14 .time-block").val(localStorage.getItem("h-14"));
+  $("#h-15 .time-block").val(localStorage.getItem("h-15"));
+  $("#h-16 .time-block").val(localStorage.getItem("h-16"));
+  $("#h-17 .time-block").val(localStorage.getItem("h-17"));
+
+  
+
 });
