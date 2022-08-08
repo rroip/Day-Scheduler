bf81b0931740089a10c70c5efc0a00476d4c578e
Stan Sitnitski <stanley@Stans-MBP.socal.rr.com>
2022-08-08 15:01:40 -0700

    updated files w/ readme and screen shot

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Work-Day-Scheduler
+# Workday Scheduler
+
+## Description
+
+A simple scheduler that anables the user to organize and save thir workday.
+
+The scheduler is interactive and highlights the time row on a schedule in relation to the current time. 
+
+
+## Usage
+
+User can find deployed app at [Workday Scheduler](https://rroip.github.io/Day-Scheduler/)
+
+![alt screen shot](./assets/images/Screen%20Shot.png)
+
+Repo for app can be found at https://github.com/rroip/Day-Scheduler
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
-  //Moment.js code for current date and time
+//Moment.js code for current date and time
   let currentDay = moment().format("MMMM, DD, YYYY");
   let displayDate = document.getElementById("currentDay");
   displayDate.innerHTML = currentDay;
   let currentHour = moment().hour();
 
+// Time management function to compare current time with with the schedule  
   $(".time-div").each(function () {
       let timeDiv = parseInt($(this).attr("id").split('-')[1]);
       
@@ -19,6 +20,7 @@ $(document).ready(function () {
       }
     });
 
+// Save button event 
     $(".saveBtn").click(function (event) {
       event.preventDefault();
       let value = $(this).siblings(".time-block").val();
@@ -26,6 +28,7 @@ $(document).ready(function () {
       localStorage.setItem(time, value);
     });
 
+// Returns saved text from the local storage    
   $("#h-9 .time-block").val(localStorage.getItem("h-9"));
   $("#h-10 .time-block").val(localStorage.getItem("h-10"));
   $("#h-11 .time-block").val(localStorage.getItem("h-11"));
@@ -35,7 +38,5 @@ $(document).ready(function () {
   $("#h-15 .time-block").val(localStorage.getItem("h-15"));
   $("#h-16 .time-block").val(localStorage.getItem("h-16"));
   $("#h-17 .time-block").val(localStorage.getItem("h-17"));
-
   
-
 });
